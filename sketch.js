51bba0fb94784da9e9989deddf0ec7c44f7a1b6f
utkarsh-agarwala <utkarsh.agarwala23@gmail.com>
2020-08-10 16:57:16 +0530

    Changed README file

--- sketch.js
+++ sketch.js
@@ -34,6 +34,8 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
+    platform = new Ground(150,305,300,170)
+
     bird = new Bird(100,100);
 
 }
@@ -58,6 +60,8 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+
+    platform.display();
 
     bird.display();
     platform.display();
